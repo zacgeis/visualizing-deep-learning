4ae659ef6ef26ecc062b5f5299b10bc350645aa8
zacgeis <zacgeis@gmail.com>
2017-04-29 19:53:30 -0500

    Make slider more clear

--- build/bundle.js
+++ build/bundle.js
@@ -469,6 +469,7 @@ var SingleLayerDisplay = (function (_super) {
         var newState = generateNetworkState(this.state.tempCount, this.state.tempLearningRate, this.state.tempHiddenLayerSize);
         this.setState({
             count: this.state.tempCount,
+            iteration: this.state.tempCount,
             learningRate: this.state.tempLearningRate,
             networkState: newState,
             hiddenLayerSize: this.state.tempHiddenLayerSize
